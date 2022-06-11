c365c47a10e0fb06a74249080e54feb72d8c0519
Kuldeep <36533806+borakuldeep@users.noreply.github.com>
2022-06-11 10:03:36 +0200

    linting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react'
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
 import theme from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider theme={theme}>
-      <App />
+    <App />
   </ChakraProvider>
 );
-
